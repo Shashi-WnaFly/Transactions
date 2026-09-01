b1e5b8e5a415c6db34d9c079acdc2b760704d559
Shashi-Anand <shashianand2600@gmail.com>
2026-09-01 19:30:21 +0530

    add getAllAccountsController or get /api/accounts

--- src/controllers/account.controller.ts
+++ src/controllers/account.controller.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
-import accountModel from "../models/account.model.js";
+import AccountModel from "../models/account.model.js";
 
 /**
  * - creating an account controller
@@ -8,11 +8,11 @@ import accountModel from "../models/account.model.js";
  * @param res
  */
 
-async function createAccountController (req: Request, res: Response) {
+async function createAccountController(req: Request, res: Response) {
   try {
     const user = req.user;
 
-    const newAccount = await accountModel.create({
+    const newAccount = await AccountModel.create({
       user: user._id,
     });
 
@@ -27,6 +27,30 @@ async function createAccountController (req: Request, res: Response) {
       error: error instanceof Error ? error.message : "Unknown error",
     });
   }
-};
+}
 
-export { createAccountController };
+async function getAllAccountsController(req: Request, res: Response) {
+  try {
+    const accountsDetails = await AccountModel.find({ user: req.user._id });
+
+    if (accountsDetails.length === 0) {
+      return res.status(500).json({
+        message: "You haven't create any account yet",
+      });
+    }
+
+    res.status(200).json({
+      status: "success",
+      accounts: accountsDetails,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(501).json({
+      message: "Something went wrong",
+      status: "failed",
+      error: error,
+    });
+  }
+}
+
+export { createAccountController, getAllAccountsController };
--- src/routes/account.routes.ts
+++ src/routes/account.routes.ts
@@ -1,11 +1,19 @@
 import express from "express";
 import userAuthMiddleware from "../middleware/auth.middleware.js";
-import { createAccountController } from "../controllers/account.controller.js";
+import { createAccountController, getAllAccountsController } from "../controllers/account.controller.js";
 
-const router = express.Router();
+const accountRouter = express.Router();
+
 /**
  * POST /api/accounts/
  */
-router.post("/", userAuthMiddleware, createAccountController);
 
-export default router;
+accountRouter.post("/", userAuthMiddleware, createAccountController);
+
+/**
+ * GET /api/accounts
+ */
+
+accountRouter.get("/", userAuthMiddleware, getAllAccountsController);
+
+export default accountRouter;
